9e7c00412256abf82b3e06edbaad8a09cb36973a
Saeedzxz123 <saeedbh1190@gmail.com>
2025-12-28 10:30:05 +0300

    setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+mongoose.connect(process.env.MONGODB_URL);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB `);
+});
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes go here
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
