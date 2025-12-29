8eb1a008b36f5890c93bfbb9891a7581c1574823
Saeedzxz123 <saeedbh1190@gmail.com>
2025-12-29 09:59:02 +0300

    add update

--- controllers/pets.js
+++ controllers/pets.js
@@ -64,7 +64,20 @@ catch(err){
   res.status(500).json({error: 'pet cant be deleted'})}
 })
 
+router.put('/:id', async (req,res)=>{
+    try{
+    const {id} = req.params
+    const pet = await Pet.findByIdAndUpdate(id, req.body, {new:true})
+    if(!pet){res.status(404).json({error:'pet not found'})}
+    else{res.status(200).json({pet})}
+    }
+    catch(err){
+        console.log(err)
+          res.status(500).json({error: 'pet not found'})
+        }
 
+    }
+)
 
 
 
